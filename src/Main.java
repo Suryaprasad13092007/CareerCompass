3b8213e59b4b187294b0fad4f41bbf77582d4651
Achyutha Naga Venkata Satya Suryaprasad <suryaprasad.25bce8538@vitapstudent.ac.in>
2026-06-18 22:56:56 +0530

    Day 6 : Added user input using Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to Career Compass");
+        
+        Scanner sc = new Scanner(System.in);
 
-        Student student = new Student("Surya","CSE",1,"Software Engineering");
-        System.out.println("Student object created sucessfully");
+        System.out.println(" Enter name : ");
+        String name = sc.nextLine();
 
+        System.out.println(" Enter Branch : ");
+        String branch = sc.nextLine();
+
+        System.out.println(" Enter year : ");
+        int year = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println(" Enter interest : ");
+        String interest = sc.nextLine();
+
+
+
+        Student student = new Student(name,branch,year,interest);
+        
         student.displayStudentInfo();
 
         CareerPath careerPath = new CareerPath("Software Engineer","Designs and develops software applications.");
