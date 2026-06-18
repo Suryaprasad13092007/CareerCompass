be7074a034489b3ce0ddce42df626fb4c07022f4
Achyutha Naga Venkata Satya Suryaprasad <suryaprasad.25bce8538@vitapstudent.ac.in>
2026-06-18 23:03:41 +0530

    Day 7 : Added ArrayList for student storage

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to Career Compass");
-        
         Scanner sc = new Scanner(System.in);
+        
 
         System.out.println(" Enter name : ");
         String name = sc.nextLine();
@@ -19,10 +20,13 @@ public class Main {
         String interest = sc.nextLine();
 
 
-
+        ArrayList<Student> students = new ArrayList<>();
         Student student = new Student(name,branch,year,interest);
-        
-        student.displayStudentInfo();
+        students.add(student);
+
+        for(Student s : students){
+            student.displayStudentInfo();
+        }
 
         CareerPath careerPath = new CareerPath("Software Engineer","Designs and develops software applications.");
 
