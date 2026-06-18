a8bb762eda5ddcf03d8d84d3f32c144f7ac65396
Achyutha Naga Venkata Satya Suryaprasad <suryaprasad.25bce8538@vitapstudent.ac.in>
2026-06-18 23:12:08 +0530

    Day 8 : Added support for multiple students

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to Career Compass");
         Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        System.out.println(" How many students do you want to add ? ");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        for(int i =1 ; i <= count ; i++){
         
+        System.out.println("\nStudent " + i);
 
         System.out.println(" Enter name : ");
         String name = sc.nextLine();
@@ -20,12 +28,15 @@ public class Main {
         String interest = sc.nextLine();
 
 
-        ArrayList<Student> students = new ArrayList<>();
         Student student = new Student(name,branch,year,interest);
         students.add(student);
+        }
+
+        System.out.println("\n====== Student List ======");
 
         for(Student s : students){
-            student.displayStudentInfo();
+            s.displayStudentInfo();
+            System.err.println();
         }
 
         CareerPath careerPath = new CareerPath("Software Engineer","Designs and develops software applications.");
